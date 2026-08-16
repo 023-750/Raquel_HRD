7898f293a85f6a931198b5b6834b378c0f9da989
JUS10 <023-750@cefi.edu.ph>
2026-08-16 21:37:52 +0800

    fix CSS and JS static asset headers

--- api/index.php
+++ api/index.php
@@ -4,6 +4,33 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $file = __DIR__ . '/..' . $uri;
 
 if ($uri !== '/' && file_exists($file) && !is_dir($file)) {
+    $ext = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+    
+    // Static assets MIME types
+    $mimes = [
+        'css'   => 'text/css; charset=UTF-8',
+        'js'    => 'application/javascript; charset=UTF-8',
+        'png'   => 'image/png',
+        'jpg'   => 'image/jpeg',
+        'jpeg'  => 'image/jpeg',
+        'gif'   => 'image/gif',
+        'svg'   => 'image/svg+xml',
+        'webp'  => 'image/webp',
+        'ico'   => 'image/x-icon',
+        'woff'  => 'font/woff',
+        'woff2' => 'font/woff2',
+        'ttf'   => 'font/ttf',
+        'eot'   => 'application/vnd.ms-fontobject',
+        'json'  => 'application/json'
+    ];
+    
+    if (isset($mimes[$ext])) {
+        header("Content-Type: " . $mimes[$ext]);
+        header("Cache-Control: public, max-age=3600");
+        readfile($file);
+        exit;
+    }
+    
     require $file;
 } else {
     require __DIR__ . '/../index.php';
